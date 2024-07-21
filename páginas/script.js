54a8149a0c58c6fb3ca90b95f01a99f475736b94
auyber <160794306+auyber@users.noreply.github.com>
2024-07-21 12:18:33 -0300

    feat: add carrousel

--- páginas/script.js
+++ páginas/script.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.carousel-slides img');
+    const totalSlides = slides.length;
+    let slideIndex = 0;
+    const intervalTime = 5000; // Slowing down the interval to 5 seconds
+
+    function showSlide(index) {
+        if (index >= totalSlides) {
+            slideIndex = 0;
+        } else if (index < 0) {
+            slideIndex = totalSlides - 1;
+        } else {
+            slideIndex = index;
+        }
+        document.querySelector('.carousel-slides').style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+
+    function showNextSlide() {
+        showSlide(slideIndex + 1);
+    }
+
+    function showPrevSlide() {
+        showSlide(slideIndex - 1);
+    }
+
+    document.querySelector('.next').addEventListener('click', showNextSlide);
+    document.querySelector('.prev').addEventListener('click', showPrevSlide);
+
+    setInterval(showNextSlide, intervalTime);
+});
+
+
 document.getElementById('formulario-comentario').addEventListener('submit', function(event) {
     event.preventDefault();
     var comentario = document.getElementById('comentario').value;
